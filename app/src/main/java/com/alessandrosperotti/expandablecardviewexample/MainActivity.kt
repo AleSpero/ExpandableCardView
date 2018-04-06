7a8b2ed53cc0e14c92b008081e3d19dc25f70358
Alessandro Sperotti <alessandro.sperotti@dsgroup.it>
2018-04-07 01:32:43 +0200

    updated sample app

--- app/src/main/java/com/alessandrosperotti/expandablecardviewexample/MainActivity.kt
+++ app/src/main/java/com/alessandrosperotti/expandablecardviewexample/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.alessandrosperotti.expandablecardviewexample
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.CollapsingToolbarLayout
+import android.support.v7.widget.Toolbar
 import android.view.View
 import android.widget.Toast
 import com.alespero.expandablecardview.ExpandableCardView
@@ -11,6 +15,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val collapsingToolbarLayout = findViewById<CollapsingToolbarLayout>(R.id.ctl)
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE)
+        collapsingToolbarLayout.setExpandedTitleTextColor(ColorStateList.valueOf(Color.WHITE))
 
         val card : ExpandableCardView = findViewById(R.id.profile)
 
